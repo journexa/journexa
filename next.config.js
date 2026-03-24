b1c2328ca6dd7d070121e97f88aed0c550dd709b
journexa <journexa@gmail.com>
2026-03-24 17:08:03 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-@type {import('next').NextConfig}
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    runtime: 'edge',
-  },
-}
-module.exports = nextConfig
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
